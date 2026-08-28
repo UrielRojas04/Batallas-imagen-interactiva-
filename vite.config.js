8decc0cd29d645e8984bb2e3aaff7eea024f0d04
UrielRojas04 <jaurojas2018@gmail.com>
2026-08-28 15:12:38 -0300

    feat: initialize git repositories for agent skills and update documentation and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/batallas-ilustracion/',
+  base: '/Batallas-imagen-interactiva-/',
   plugins: [react()],
 })
